9ed417d9b3a140f0325b6102f4081e83d292aa09
calvin <calvinducharme@gmail.com>
2023-09-11 15:02:58 -0700

    sw??

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import "@/styles/globals.css"
 import { Metadata } from "next"
 
@@ -6,6 +8,7 @@ import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { useEffect } from "react";
 
 export const metadata: Metadata = {
   title: {
@@ -29,6 +32,21 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(
+          function(registration) {
+            console.log('Service Worker registration successful with scope: ', registration.scope);
+          }, 
+          function(err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  },[])
+  
   return (
     <>
       <html lang="en" suppressHydrationWarning>
