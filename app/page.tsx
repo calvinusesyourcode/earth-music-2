51d5bb132b599dcf276949464f3c5aab588de480
calvin <calvinducharme@gmail.com>
2023-09-11 14:54:18 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/card"
 import { useContext, useEffect, useState } from "react";
 
-export function shuffleArray(array: any) {
+function shuffleArray(array: any) {
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
